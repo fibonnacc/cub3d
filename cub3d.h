f00330bc0a88e574b3f63a6e6144f008e779bc88
Fibo <helfatih@Fibo.elfatih>
2025-11-03 07:10:55 -0500

    hello

--- cub3d.h
+++ cub3d.h
@@ -14,6 +14,23 @@ typedef struct s_cub3d
   struct s_cub3d *next;
 } t_cub3d;
 
+typedef
+
+typedef struct s_data
+{
+  void *mlx;
+  void *img;
+  void  *win;
+  char  *addr;
+  int   line_length;
+  int   bits_per_pixels;
+  int   endian;
+  t_joueur  player;
+  char  **maps;
+  int   map_widths;
+  int   map_height;
+} t_data;
+
 void  add_back(t_cub3d **list, t_cub3d *new);
 t_cub3d *new_node(char *content);
 
---
+++ init.c
@@ -0,0 +1,6 @@
+#include "cub3d.h"
+
+void init_mlx(t_data *data)
+{
+  data->mlx =  mlx_init();
+}
